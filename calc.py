0792bbb8bb82b78c2badb789fb997a2dd8b42be7
vensder <vensder@gmail.com>
2018-07-11 22:22:26 +0300

    added minus

--- calc.py
+++ calc.py
@@ -43,13 +43,24 @@ def click_plus():
     operation_sign.configure(text=operation)
 
 
+def click_minus():
+    global operation_sign
+    global operation
+    global operand_2_value
+    operation = '-'
+    operand_2_value = '0'
+    operation_sign.configure(text=operation)
+
+
 def click_eq():
     global operand_1_value
     global operand_2_value
     if operation == '+' and operand_2_value:
         operand_1_value = str(int(operand_1_value) + int(operand_2_value))
-        operand_2_value = ''
+    elif operation == '-' and operand_2_value:
+        operand_1_value = str(int(operand_1_value) - int(operand_2_value))
 
+    operand_2_value = ''
     operand_1_label.configure(text=operand_1_value)
     operand_2_label.configure(text=operand_2_value)
 
@@ -84,7 +95,9 @@ button_2 = ttk.Button(window, text="2", command=click_2)
 button_3 = ttk.Button(window, text="3", command=click_3)
 button_4 = ttk.Button(window, text="4", command=click_4)
 button_5 = ttk.Button(window, text="5", command=click_5)
+
 button_plus = ttk.Button(window, text='+', command=click_plus)
+button_minus = ttk.Button(window, text='-', command=click_minus)
 button_eq = ttk.Button(window, text='=', command=click_eq)
 
 button_0.grid(column=1, row=2)
@@ -94,7 +107,8 @@ button_3.grid(column=1, row=3)
 button_4.grid(column=2, row=3)
 button_5.grid(column=3, row=3)
 button_plus.grid(column=4, row=2)
-button_eq.grid(column=4, row=3)
+button_minus.grid(column=4, row=3)
+button_eq.grid(column=4, row=4)
 
 
 window.mainloop()
