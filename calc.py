5b818f99342f0c0fdd81a2140de478a532908a29
vensder <vensder@gmail.com>
2018-07-12 22:40:45 +0300

    added lambda for operations, added common operation eval

--- calc.py
+++ calc.py
@@ -8,7 +8,7 @@ window.title("Calculator")
 window.resizable(False, False)
 operand_1_value = '0'
 operand_2_value = ''
-operation = ''
+operation_sign = ''
 
 operand_1_label = ttk.Label(window, text='0')
 operand_1_label.grid(column=1, row=0, columnspan=3, sticky='e')
@@ -16,14 +16,13 @@ operand_1_label.grid(column=1, row=0, columnspan=3, sticky='e')
 operand_2_label = ttk.Label(window, text='')
 operand_2_label.grid(column=1, row=1, columnspan=3, sticky='e')
 
-operation_sign = ttk.Label(window, text=operation)
-operation_sign.grid(column=4, row=0, rowspan=2)
+operation_sign_label = ttk.Label(window, text=operation_sign)
+operation_sign_label.grid(column=4, row=0, rowspan=2)
 
 
 def append_digit(digit):
     global operand_1_value
     global operand_2_value
-
     if operand_2_value:
         operand_2_value += str(digit)
         operand_2_value = str(int(operand_2_value))
@@ -34,55 +33,22 @@ def append_digit(digit):
         operand_1_label.configure(text=operand_1_value)
 
 
-def click_plus():
+def click_operation_sign(op):
+    global operation_sign_label
     global operation_sign
-    global operation
     global operand_2_value
-    operation = '+'
+    operation_sign = op
     operand_2_value = '0'
-    operation_sign.configure(text=operation)
+    operation_sign_label.configure(text=operation_sign)
 
 
-def click_minus():
-    global operation_sign
-    global operation
-    global operand_2_value
-    operation = '-'
-    operand_2_value = '0'
-    operation_sign.configure(text=operation)
-
-
-def click_mult():
-    global operation_sign
-    global operation
-    global operand_2_value
-    operation = '*'
-    operand_2_value = '0'
-    operation_sign.configure(text=operation)
-
-
-def click_div():
-    global operation_sign
-    global operation
-    global operand_2_value
-    operation = '/'
-    operand_2_value = '0'
-    operation_sign.configure(text=operation)
-
-
-def click_eq():
+def eval_operation():
     global operand_1_value
     global operand_2_value
-
-    if operation == '+' and operand_2_value:
-        operand_1_value = str(int(operand_1_value) + int(operand_2_value))
-    elif operation == '-' and operand_2_value:
-        operand_1_value = str(int(operand_1_value) - int(operand_2_value))
-    elif operation == '*' and operand_2_value:
-        operand_1_value = str(int(operand_1_value) * int(operand_2_value))
-    elif operation == '/' and operand_2_value:
-        operand_1_value = str(int(operand_1_value) / int(operand_2_value))
-
+    global operation_sign
+    operand_1_value = str(eval(operand_1_value +
+                               operation_sign +
+                               operand_2_value))
     operand_2_value = ''
     operand_1_label.configure(text=operand_1_value)
     operand_2_label.configure(text=operand_2_value)
@@ -93,10 +59,10 @@ def click_clear():
     global operand_2_value
     operand_1_value = '0'
     operand_2_value = ''
-    operation = ''
+    operation_sign = ''
     operand_1_label.configure(text=operand_1_value)
     operand_2_label.configure(text=operand_2_value)
-    operation_sign.configure(text=operation)
+    operation_sign_label.configure(text=operation_sign)
 
 
 def digit_buttons_gen(n):
@@ -123,19 +89,25 @@ digit_buttons[8].grid(column=3, row=4)
 digit_buttons[9].grid(column=1, row=5)
 
 
-button_mult = ttk.Button(window, text='*', command=click_mult)
-button_div = ttk.Button(window, text='/', command=click_div)
-button_plus = ttk.Button(window, text='+', command=click_plus)
-button_minus = ttk.Button(window, text='-', command=click_minus)
-button_eq = ttk.Button(window, text='=', command=click_eq)
-button_clear = ttk.Button(window, text='Clear', command=click_clear)
+button_mult = ttk.Button(window, text='*',
+                         command=lambda: click_operation_sign('*'))
+button_div = ttk.Button(window, text='/',
+                        command=lambda: click_operation_sign('/'))
+button_plus = ttk.Button(window, text='+',
+                         command=lambda: click_operation_sign('+'))
+button_minus = ttk.Button(window, text='-',
+                          command=lambda: click_operation_sign('-'))
+button_eq = ttk.Button(window, text='=',
+                       command=eval_operation)
+button_clear = ttk.Button(window, text='Clear',
+                          command=click_clear)
 
 button_mult.grid(column=2, row=5)
 button_div.grid(column=3, row=5)
-button_plus.grid(column=4, row=2)
-button_minus.grid(column=4, row=3)
-button_eq.grid(column=4, row=4)
-button_clear.grid(column=4, row=5)
+button_plus.grid(column=4, row=3)
+button_minus.grid(column=4, row=4)
+button_eq.grid(column=4, row=5)
+button_clear.grid(column=4, row=2)
 
 
 window.mainloop()
