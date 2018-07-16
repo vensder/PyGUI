272350205ced53053982c2392970c536605396f4
vensder <vensder@gmail.com>
2018-07-16 14:11:49 +0300

    changed elements placed

--- timer.py
+++ timer.py
@@ -18,6 +18,10 @@ mm.set('00')
 ss.set('00')
 
 
+def hh_mm_ss_to_sec():
+    return int(hh.get()) * 60 * 60 + int(mm.get()) * 60 + int(ss.get())
+
+
 def x_0_plus(obj, limit):
     x_0 = int(obj.get()[0])
     x_0 += 1
@@ -50,60 +54,60 @@ def x_1_minus(obj, limit):
     obj.set(obj.get()[0] + str(x_1))
 
 
-hh_label = tk.Label(window, text='hh:', font=fixed_font)
-hh_label.grid(column=0, row=0, rowspan=2)
-mm_label = tk.Label(window, text='mm:', font=fixed_font)
-mm_label.grid(column=0, row=2, rowspan=2)
-ss_label = tk.Label(window, text='ss:', font=fixed_font)
-ss_label.grid(column=0, row=4, rowspan=2)
+hh_label = tk.Label(window, text='hh', font=fixed_font)
+hh_label.grid(column=2, row=0, columnspan=2)
+mm_label = tk.Label(window, text='mm', font=fixed_font)
+mm_label.grid(column=4, row=0, columnspan=2)
+ss_label = tk.Label(window, text='ss', font=fixed_font)
+ss_label.grid(column=6, row=0, columnspan=2)
 
 hh_label_value = tk.Label(window, textvariable=hh, font=fixed_font)
-hh_label_value.grid(column=1, row=0, rowspan=2)
+hh_label_value.grid(column=2, row=3, columnspan=2)
 mm_label_value = tk.Label(window, textvariable=mm, font=fixed_font)
-mm_label_value.grid(column=1, row=2, rowspan=2)
+mm_label_value.grid(column=4, row=3, columnspan=2)
 ss_label_value = tk.Label(window, textvariable=ss, font=fixed_font)
-ss_label_value.grid(column=1, row=4, rowspan=2)
+ss_label_value.grid(column=6, row=3, columnspan=2)
 
 hh_button_plus_0 = tk.Button(window, text='+', font=fixed_font,
                              command=lambda: x_0_plus(hh, 9))
 hh_button_minus_0 = tk.Button(window, text='-', font=fixed_font,
                               command=lambda: x_0_minus(hh, 9))
-hh_button_plus_0.grid(column=2, row=0)
-hh_button_minus_0.grid(column=2, row=1)
+hh_button_plus_0.grid(column=2, row=1)
+hh_button_minus_0.grid(column=2, row=4)
 
 hh_button_plus_1 = tk.Button(window, text='+', font=fixed_font,
                              command=lambda: x_1_plus(hh, 9))
 hh_button_minus_1 = tk.Button(window, text='-', font=fixed_font,
                               command=lambda: x_1_minus(hh, 9))
-hh_button_plus_1.grid(column=3, row=0)
-hh_button_minus_1.grid(column=3, row=1)
+hh_button_plus_1.grid(column=3, row=1)
+hh_button_minus_1.grid(column=3, row=4)
 
 mm_button_plus_0 = tk.Button(window, text='+', font=fixed_font,
                              command=lambda: x_0_plus(mm, 5))
 mm_button_minus_0 = tk.Button(window, text='-', font=fixed_font,
                               command=lambda: x_0_minus(mm, 5))
-mm_button_plus_0.grid(column=2, row=2)
-mm_button_minus_0.grid(column=2, row=3)
+mm_button_plus_0.grid(column=4, row=1)
+mm_button_minus_0.grid(column=4, row=4)
 
 mm_button_plus_1 = tk.Button(window, text='+', font=fixed_font,
                              command=lambda: x_1_plus(mm, 9))
 mm_button_minus_1 = tk.Button(window, text='-', font=fixed_font,
                               command=lambda: x_1_minus(mm, 9))
-mm_button_plus_1.grid(column=3, row=2)
-mm_button_minus_1.grid(column=3, row=3)
+mm_button_plus_1.grid(column=5, row=1)
+mm_button_minus_1.grid(column=5, row=4)
 
 ss_button_plus_0 = tk.Button(window, text='+', font=fixed_font,
                              command=lambda: x_0_plus(ss, 5))
 ss_button_minus_0 = tk.Button(window, text='-', font=fixed_font,
                               command=lambda: x_0_minus(ss, 5))
-ss_button_plus_0.grid(column=2, row=4)
-ss_button_minus_0.grid(column=2, row=5)
+ss_button_plus_0.grid(column=6, row=1)
+ss_button_minus_0.grid(column=6, row=4)
 
 ss_button_plus_1 = tk.Button(window, text='+', font=fixed_font,
                              command=lambda: x_1_plus(ss, 9))
 ss_button_minus_1 = tk.Button(window, text='-', font=fixed_font,
                               command=lambda: x_1_minus(ss, 9))
-ss_button_plus_1.grid(column=3, row=4)
-ss_button_minus_1.grid(column=3, row=5)
+ss_button_plus_1.grid(column=7, row=1)
+ss_button_minus_1.grid(column=7, row=4)
 
 window.mainloop()
